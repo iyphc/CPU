239557b5173e3a6ba8a77d6aed522499e234d1e3
iyphc <werhistudy@yandex.ru>
2022-05-12 12:53:24 +0300

    Uses all the functionality

--- sum.asm
+++ sum.asm
@@ -1,33 +1,15 @@
 push 10
 push 15
-
-
-
-
+push 100
 pop ax
 pop bx
+pop cx
 add ax bx
 add ax bx
-add ax bx
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-add ax bx
-mov ax bx
-
-
-
-
-
+add ax cx
+add ax cx
+add cx ax
+add cx bx
+mov dx ax
+push 100
+pop ax
